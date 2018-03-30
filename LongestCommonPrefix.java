51a1799dd9dcbbead44559b41210de40627050dd
xiaolei hu <1245976894@qq.com>
2018-03-30 22:13:44 +0800

    format the code.

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -36,18 +36,18 @@ public class LongestCommonPrefix {
 //            return charWord.trim();
 //        }
 //        return strs[0];
-        if(strs == null || strs.length==0) return "";
-        String prifex = strs[0];        // 默认将第一个认为是最长共同
-        for(int i=1;i<strs.length;i++){
+        if(strs == null || strs.length == 0) return "";
+        String prifex = strs[0];        // 默认将第一个认为是最长共同字符串
+        for(int i = 1; i < strs.length; i++) {
             // 将当前最长共同字符串和当前数组中的对比,把小的那个作为长度
-            int len = prifex.length()>strs[i].length() ? strs[i].length():prifex.length();
-            int j=0;
-            for(j=0;j<len;j++){
+            int len = prifex.length() > strs[i].length() ? strs[i].length():prifex.length();
+            int j = 0;
+            for(j = 0; j < len; j++) {
                 // 逐个字符比较,不等的时候退出
-                if(prifex.charAt(j)!=strs[i].charAt(j))
+                if(prifex.charAt(j) != strs[i].charAt(j))
                     break;
             }// 退出的j即当前的最小,整个遍历结束后就是整个的最小了
-            prifex = prifex.substring(0,j);
+            prifex = prifex.substring(0, j);
         }
         return prifex;
     }
