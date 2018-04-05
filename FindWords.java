5e5ac6ec0c6e48949fde6ea20037e757dad06b27
xiaolei hu <1245976894@qq.com>
2018-04-05 21:49:33 +0800

    键盘行

---
+++ FindWords.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindWords {
+   // public String[] findWords(String[] words) {
+        //String firstLine = "QWERTYUIOP", secondLine = "ASDFGHJKL", thirdLine = "ZXCVBNM";
+        //String[] word = new String[]{"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+//        char[] row1 = new char[]{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+//        char[] row2 = new char[]{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+//        char[] row3 = new char[]{'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+//        String[] wordList = null;
+//        //int wordsLen = words.length, wordLen = word.length;
+//        for (String word : words) {
+//            int one = 0, two = 0, three = 0;
+//            for (char c : word.toCharArray()) {
+//                if (row1.)
+//            }
+//        }
+//        return wordList;
+   // }
+
+//    public boolean isValid(String words) {
+//        String[] word = new String[]{"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+//        //List<String> wordList = new ArrayList<>();
+//        int wordsLen = words.length(), wordLen = word.length;
+//        //int singleWordLen = words[i].length();
+//        char[] charList = words.toCharArray();
+//        for (int j = 0; j < wordLen; j++) {
+//            for (int m = 0; m < charList.length; m++) {
+//                if (!word[j].contains(String.valueOf(charList[m]))){
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
