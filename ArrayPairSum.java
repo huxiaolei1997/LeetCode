bf20c91e8210b39c7c1278cdc53274c8d5ab0556
xiaolei hu <1245976894@qq.com>
2018-04-09 18:13:20 +0800

    format code

--- ArrayPairSum.java
+++ ArrayPairSum.java
@@ -8,9 +8,9 @@ public class ArrayPairSum {
         System.out.println(Arrays.toString(nums));
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-           if (i % 2 == 0) {
-               sum += nums[i];
-           }
+            if (i % 2 == 0) {
+                sum += nums[i];
+            }
         }
         return sum;
     }
