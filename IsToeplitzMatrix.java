66c3b482758cdff299893f246b45fa8a26a6adf3
huxiaolei1997 <huxiaolei1997@gmail.com>
2018-05-30 22:27:06 +0800

    如果一个矩阵的每一方向由左上到右下的对角线上具有相同元素，那么这个矩阵是托普利茨矩阵。
    给定一个 M x N 的矩阵，当且仅当它是托普利茨矩阵时返回 True。

---
+++ IsToeplitzMatrix.java
@@ -0,0 +1,22 @@
+/**
+ * @author xiaolei hu
+ * @date 2018/5/30 22:13
+ **/
+public class IsToeplitzMatrix {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < m - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
